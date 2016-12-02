9b3b56ca63502e65cc38bb64cf7c67c3bb107742
Ghilt <Adam.n.a3@gmail.com>
2016-12-02 14:59:24 +0100

    Second problem of day2 AdventOfCode2016 http://adventofcode.com/2016/day/2

--- MixMiscBag/AdventOfCode2016/Day2.cs
+++ MixMiscBag/AdventOfCode2016/Day2.cs
@@ -8,46 +8,77 @@ namespace AdventOfCode2016
 {
     class Day2
     {
-        private int x = 1;
-        private int y = 1;
-        int[,] keyPad = new int[3, 3] { { 1, 4, 7 }, { 2, 5, 8 }, { 3, 6, 9 } };
+        AdventParser parser;
+        private int x;
+        private int y;
+        KeyValuePair<char[,], Func<int, int, bool>> keyPad1 = new KeyValuePair<char[,], Func<int, int, bool>>(new char[3, 3] { 
+        { '1', '4', '7' }, 
+        { '2', '5', '8' }, 
+        { '3', '6', '9' } },
+        (x, y) =>
+            (x > 2) ||
+            (x < 0) ||
+            (y > 2) ||
+            (y < 0));
+        KeyValuePair<char[,], Func<int, int, bool>> keyPad2 = new KeyValuePair<char[,], Func<int, int, bool>>(new char[5, 5] { 
+        { '0', '0', '5', '0', '0' }, 
+        { '0', '2', '6', 'A', '0' }, 
+        { '1', '3', '7', 'B', 'D' }, 
+        { '0', '4', '8', 'C', '0' }, 
+        { '0', '0', '9', '0', '0' } },
+        (x, y) =>
+            (x != 2 && (y == 0 || y == 4)) ||
+            (y != 2 && (x == 0 || x == 4)) ||
+            (x > 4) ||
+            (x < 0) ||
+            (y > 4) ||
+            (y < 0));
+
         string result = "";
 
         public Day2(AdventParser parser)
         {
-            string[] data = parser.Parse("input_day2.txt", "\n");
+            this.parser = parser;
+            x = 1;
+            y = 1;
+            Solve(keyPad1);
+            x = 0;
+            y = 2;
+            Solve(keyPad2);
+        }
 
+        private void Solve(KeyValuePair<char[,], Func<int, int, bool>> keyPad)
+        {
+            string[] data = parser.Parse("input_day2.txt", "\n");
             foreach (string instruction in data)
             {
                 if (instruction.Equals(""))
                 {
-                    return;
+                    continue;
                 }
                 for (int i = 0; i < instruction.Length; i++)
                 {
                     switch (instruction[i])
                     {
                         case 'U':
-                            if(y != 0) y -= 1;
+                            if (!keyPad.Value(x, y - 1)) y -= 1;
                             break;
                         case 'R':
-                            if (x != 2) x += 1;
+                            if (!keyPad.Value(x + 1, y)) x += 1;
                             break;
                         case 'D':
-                            if (y != 2) y += 1;
+                            if (!keyPad.Value(x, y + 1)) y += 1;
                             break;
                         case 'L':
-                            if (x != 0) x -= 1;
+                            if (!keyPad.Value(x - 1, y)) x -= 1;
                             break;
                         default:
                             break;
-
                     }
                 }
-                result += keyPad[x,y];
-               
+                result += keyPad.Key[x, y];
             }
-        
+            result += "\n";
         }
 
         internal void PrintSolution()
