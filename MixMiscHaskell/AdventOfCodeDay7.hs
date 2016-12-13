6b568895e7485fac4f075e17f27422e0fd4945d3
Ghilt <Adam.n.a3@gmail.com>
2016-12-13 16:40:12 +0100

    Problem Day7 AdventOfCode2016
    http://adventofcode.com/2016/day/7

---
+++ MixMiscHaskell/AdventOfCodeDay7.hs
@@ -0,0 +1,29 @@
+--cabal install regex-posix
+--import Text.Regex.Posix Could not get backreferences in the regexp to work
+import Data.List.Split
+
+splitOnBrackets :: String -> [String] 
+splitOnBrackets str = concat ([splitOn "]" x | x <- splitOn "[" str])
+
+checkExistPattern :: String -> Bool
+checkExistPattern [] = False 
+checkExistPattern (_:_:_:[]) = False
+checkExistPattern (a:b:c:d:str) = (a /= b) && (a == d) && (b == c) || (checkExistPattern (b:c:d:str))
+
+checkValid :: [String] -> Bool
+checkValid strs = let (good, bad) = isValid strs in not bad && good
+
+isValid :: [String] -> (Bool, Bool)
+isValid [] = (False, False)
+isValid (x:y:xs) = let (dg, db) = isValid xs in (checkExistPattern x || dg, checkExistPattern y || db)
+isValid (x:xs) = (checkExistPattern x, False)
+
+main = do
+    putStrLn "Start..."  
+    contents <- readFile "input/day7.txt" 
+    let linesOfFile = lines contents
+    let splitLines = map splitOnBrackets linesOfFile
+    let viable = filter checkValid splitLines
+    let countViable = length viable
+    putStrLn $ "Supports TLS: " ++ show( countViable ) 
+
